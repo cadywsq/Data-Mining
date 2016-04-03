53829502934b330bdfa32545e2fc1d7a8a40e8d3
verawang28 <verawang28@gmail.com>
2016-04-03 19:10:56 -0400

    add dataset and instance

--- src/decisiontree/Attribute.java
+++ src/decisiontree/Attribute.java
@@ -1,7 +1,26 @@
 package decisiontree;
 
+import java.util.List;
+
 /**
  * Created by wangxinlei on 4/3/16.
  */
 public class Attribute {
+    public final String name;
+    public final List<String> values;
+    public final boolean numeric;
+    public final boolean label;
+
+    public Attribute(List<String> values, String name) {
+        this.name = name;
+        this.values = values;
+        if (values.size() == 1 && values.get(0).equals("real"))
+            numeric = true;
+        else
+            numeric = false;
+        if (name.equals("class"))
+            label = true;
+        else
+            label = false;
+    }
 }
--- src/decisiontree/Main.java
+++ src/decisiontree/Main.java
@@ -1,10 +1,36 @@
 package decisiontree;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by wangxinlei on 4/3/16.
  */
 public class Main {
     public static void main(String[] args) {
         // train or build tree
+    }
+
+    private static DataSet read(String filename) {
+        DataSet data = new DataSet();
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File(filename));
+        } catch (FileNotFoundException exception) {
+            System.out.println("Error: could not find file");
+            System.exit(1);
+        }
+        String line;
+        scanner.nextLine(); // Skip first line
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            if (line.equals("@data"))
+                break;
+            data.addAttribute(line);
+        }
+        while (scanner.hasNextLine())
+            data.addInstance(scanner.nextLine());
+        return data;
     }
 }
