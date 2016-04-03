f765723110f7446c5935ad6ea7ed0ff17c4b1ed2
verawang28 <verawang28@gmail.com>
2016-04-03 18:52:14 -0400

    initial structure

---
+++ src/decisiontree/Main.java
@@ -0,0 +1,10 @@
+package decisiontree;
+
+/**
+ * Created by wangxinlei on 4/3/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // train or build tree
+    }
+}
--- src/decisiontree/TreeNode.java
+++ src/decisiontree/TreeNode.java
@@ -3,5 +3,5 @@ package decisiontree;
 /**
  * Created by wangxinlei on 4/3/16.
  */
-public class Attribute {
+public class TreeNode {
 }
