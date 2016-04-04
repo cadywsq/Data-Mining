8b1160e48cea895b7c2eea7ba939cec78aa01ed7
Cady Wang <siqiw1@andrew.cmu.edu>
2016-04-03 20:38:16 -0400

    Updating DataSet.java

--- src/decisiontree/Instance.java
+++ src/decisiontree/Instance.java
@@ -1,21 +1,21 @@
 package decisiontree;
 
-import java.util.LinkedHashMap;
+import java.util.Map;
 
 // Holds data for a training instance
 public class Instance {
 	
 	public final String label;
-	public final LinkedHashMap<String, String> values;
+	public final Map<String, String> attributeValues;
 	
-	public Instance(LinkedHashMap<String, String> values, String label) {
+	public Instance(Map<String, String> attributeValues, String label) {
 		this.label = label;
-		this.values = values;
+		this.attributeValues = attributeValues;
 	}
 	
 	// Developer tool: print instance
 	public void print() {
-		System.out.println(label + ": " + values);
+		System.out.println(label + ": " + attributeValues);
 	}
 	
 
