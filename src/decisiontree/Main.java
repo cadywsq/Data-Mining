f9cc66ef9257d036ef3720374785c8097d9b0d1a
Cady Wang <siqiw1@andrew.cmu.edu>
2016-04-04 13:31:30 -0400

    adjust class of readData

--- src/decisiontree/Main.java
+++ src/decisiontree/Main.java
@@ -1,11 +1,16 @@
 package decisiontree;
 
+import java.util.ArrayList;
+
 /**
  * Created by wangxinlei on 4/3/16.
  */
 public class Main {
     public static void main(String[] args) {
         // train or build tree
+        DataSet dataSet = DataSet.readData(InfoRatio.FILE_PATH);
+        ArrayList<Attribute> attributes = dataSet.attributes;
+        ArrayList<Instance> instances = dataSet.instances;
     }
 
 }
