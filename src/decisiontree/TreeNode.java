0751e1362062820745b860ddf5695bcc9dd0b95c
verawang28 <verawang28@gmail.com>
2016-04-03 21:36:39 -0400

    complete TreeNode

--- src/decisiontree/TreeNode.java
+++ src/decisiontree/TreeNode.java
@@ -1,5 +1,6 @@
 package decisiontree;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,8 +8,55 @@ import java.util.List;
  */
 public class TreeNode {
     boolean isLeaf;
+    // @attribute: Service_type, Customer, Monthly_fee, Advertisement_budget
+    // Size, Promotion, Interest_rate, Period
     String attributeName;
+    // @attribute Label {0,1}
+    String label;
+
     List<TreeNode> children;
+
+    // if current node is split by string attribute
     String splitValue;
+    // if current node is split by numeric attribute
     double splitNum;
+
+    public TreeNode(String attributeOrLabel, boolean isLeaf) {
+        this.isLeaf = isLeaf;
+        if (isLeaf) {
+            children = null;
+            label = attributeOrLabel;
+        } else {
+            children = new ArrayList<>();
+            attributeName = attributeOrLabel;
+        }
+    }
+
+    public void addChild(TreeNode child) {
+        children.add(child);
+    }
+
+    // Prints the entire subtree  of the node
+    public void print(int depth) {
+        if (isLeaf) {
+            System.out.print(": " + label);
+        } else {
+            for (TreeNode child : children) {
+                System.out.print("\n");
+                for (int i = 0; i < depth; i++)
+                    System.out.print("|\t");
+                System.out.print(attributeName);
+                if (splitValue != null) {
+                    System.out.print(splitValue);
+                    child.print(depth + 1);
+                } else {
+                    System.out.print(child.splitNum);
+                    child.print(depth + 1);
+                }
+
+            }
+
+
+        }
+    }
 }
