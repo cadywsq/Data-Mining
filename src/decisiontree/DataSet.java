26a456f5deb08e95b07ce1a7cbf0be8958520e08
Cady Wang <siqiw1@andrew.cmu.edu>
2016-04-03 20:40:16 -0400

    Done updating DataSet.java

--- src/decisiontree/DataSet.java
+++ src/decisiontree/DataSet.java
@@ -9,29 +9,12 @@ import java.util.Map;
 public class DataSet {
 
     // Ordered list of instances
-    public ArrayList<Instance> instances;
     public ArrayList<Attribute> attributes;
+    public ArrayList<Instance> instances;
 
     public DataSet() {
-        instances = new ArrayList<>();
         attributes = new ArrayList<>();
-    }
-
-    // Add instance to data set (add all attributes before doing this)
-    public void addInstance(String line) {
-        String[] values = line.split(",");
-        Map<String, String> attributeValues = new HashMap<>();
-        for (int i = 0; i < values.length; i++) {
-            attributeValues.put(attributes.get(i).name, values[i]);
-        }
-        instances.add(attributeValues);
-//        ArrayList<String> features = new ArrayList<String>(Arrays.asList(line.split(",")));
-//        String label = features.remove(features.size() - 1);
-//        LinkedHashMap<String, String> attributeValues = new LinkedHashMap<String, String>();
-//        for (int i = 0; i < features.size(); i++) {
-//            attributeValues.put(attributes.get(i).name, features.get(i));
-//        }
-//        instances.add(new Instance(attributeValues, label));
+        instances = new ArrayList<>();
     }
 
     // Add attribute to data set
@@ -40,6 +23,16 @@ public class DataSet {
         String[] tokens = line.split(" ");
         ArrayList<String> values = new ArrayList<>(Arrays.asList(tokens[2].split("\\W+")));
         attributes.add(new Attribute(values, tokens[1]));
+    }
+
+    // Add instance to data set (add all attributes before doing this)
+    public void addInstance(String line) {
+        String[] values = line.split(",");
+        Map<String, String> attributeValues = new HashMap<>();
+        for (int i = 0; i < values.length - 1; i++) {
+            attributeValues.put(attributes.get(i).name, values[i]);
+        }
+        instances.add(new Instance(attributeValues, values[values.length - 1]));
     }
 
 }
