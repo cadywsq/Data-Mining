164e14d021ac00600a370f59bd32aa0855dd9f20
verawang28 <verawang28@gmail.com>
2016-04-03 20:37:16 -0400

    add fields in TreeNode

--- src/decisiontree/TreeNode.java
+++ src/decisiontree/TreeNode.java
@@ -1,7 +1,14 @@
 package decisiontree;
 
+import java.util.List;
+
 /**
  * Created by wangxinlei on 4/3/16.
  */
 public class TreeNode {
+    boolean isLeaf;
+    String attributeName;
+    List<TreeNode> children;
+    String splitValue;
+    double splitNum;
 }
