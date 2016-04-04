ac80b8ddaba7d21b4bcb0e484e30ceb4f3356c03
verawang28 <verawang28@gmail.com>
2016-04-04 13:28:49 -0400

    modify tree

--- src/decisiontree/Main.java
+++ src/decisiontree/Main.java
@@ -4,7 +4,7 @@ package decisiontree;
  * Created by wangxinlei on 4/3/16.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
         // train or build tree
     }
 
